44641f10b848a95fac5395d3fd6b5b52137670a1
Joakim Erdfelt <joakim@erdfelt.com>
2011-02-07 21:16:42 -0700

    Stubbing out JarListing

---
+++ sdkfido-lib/src/main/java/net/erdfelt/android/sdkfido/local/JarListing.java
@@ -0,0 +1,16 @@
+package net.erdfelt.android.sdkfido.local;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Represents a Jar Listing.
+ */
+public class JarListing extends ArrayList<String> {
+    private static final long serialVersionUID = 1L;
+
+    public JarListing(File jarfile) throws IOException {
+
+    }
+}
