020e160e2147eb44a76f74f0977ac82c2389793a
Joakim Erdfelt <joakim@erdfelt.com>
2011-02-11 19:56:37 -0700

    Making log a bit less noisy

--- sdkfido-lib/src/main/java/net/erdfelt/android/sdkfido/logging/LeanConsoleHandler.java
+++ sdkfido-lib/src/main/java/net/erdfelt/android/sdkfido/logging/LeanConsoleHandler.java
@@ -24,10 +24,15 @@ public class LeanConsoleHandler extends Handler {
     @Override
     public void publish(LogRecord record) {
         StringBuilder buf = new StringBuilder();
-        buf.append("[").append(record.getLevel().getName());
-        buf.append("] ").append(record.getLoggerName());
-        buf.append(" (").append(record.getSourceMethodName());
-        buf.append("): ").append(record.getMessage());
+        buf.append("[").append(record.getLevel().getName()).append("] ");
+        String logname = record.getLoggerName();
+        int idx = logname.lastIndexOf('.');
+        if (idx > 0) {
+            logname = logname.substring(idx + 1);
+        }
+        buf.append(logname);
+        buf.append(": ");
+        buf.append(record.getMessage());
 
         System.out.println(buf.toString());
         if (record.getThrown() != null) {
