788a7c14879dcbfc35c247188ebf29c4ea55d1cf
Joakim Erdfelt <joakim@erdfelt.com>
2011-02-19 05:15:34 -0700

    Fixing codename sort order

--- sdkfido-lib/src/main/java/net/erdfelt/android/sdkfido/sdks/NewestCodenameSorter.java
+++ sdkfido-lib/src/main/java/net/erdfelt/android/sdkfido/sdks/NewestCodenameSorter.java
@@ -2,12 +2,20 @@ package net.erdfelt.android.sdkfido.sdks;
 
 import java.util.Comparator;
 
-public class NewestCodenameSorter implements Comparator<String>{
+public class NewestCodenameSorter implements Comparator<String> {
     public static final NewestCodenameSorter INSTANCE = new NewestCodenameSorter();
-    
+
     @Override
     public int compare(String o1, String o2) {
-        // TODO Auto-generated method stub
-        return 0;
+        if ((o1 == null) && (o2 == null)) {
+            return 0;
+        }
+        if (o2 == null) {
+            return -1;
+        }
+        if (o1 == null) {
+            return 1;
+        }
+        return o2.compareTo(o1);
     }
 }
